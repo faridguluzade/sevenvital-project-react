74333e831b6448c619efc88cabffb2341fcfbae8
Farid <faridguluzade26@gmail.com>
2023-05-22 10:06:26 -0800

    guarantee section was added

--- src/components/Guarantee/Guarantee.jsx
+++ src/components/Guarantee/Guarantee.jsx
@@ -24,6 +24,31 @@ const Guarantee = () => {
             </span>
           </div>
         </Col>
+
+        <Col xs={3}>
+          <div className="guarantee__box">
+            <UilCommentHeart size="30" />
+            <span className="guarantee__text">
+              Over 100 000 satisfied customers
+            </span>
+          </div>
+        </Col>
+        <Col xs={3}>
+          <div className="guarantee__box">
+            <UilTruck size="30" />
+            <span className="guarantee__text">
+              Free delivery for orders over $50
+            </span>
+          </div>
+        </Col>
+        <Col xs={3}>
+          <div className="guarantee__box">
+            <UilHeadphones size="30" />
+            <span className="guarantee__text">
+              24/7 online customers support
+            </span>
+          </div>
+        </Col>
       </Row>
     </Container>
   );
