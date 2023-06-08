5bd5b1cd6928509ec47099b47bfd4b69c6aa5ed0
faridguluzade <tu5mhbxzt.code.edu.az>
2023-06-08 11:11:43 -0800

    creatig custom useScrool hooks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation/MainNavigation";
 import Footer from "../components/Footer/Footer";
+import { useScroll } from "../hooks/useScroll";
 
 function Home() {
+  useScroll();
   return (
     <>
       <MainNavigation />
